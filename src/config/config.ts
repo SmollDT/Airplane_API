73faf311faceec49d461d572b4fe86e397c1e50f
Zohaib <145664940+SmollDT@users.noreply.github.com>
2024-04-25 20:28:58 +0000

    feat: addition of mnemonic.

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,8 +4,8 @@ const algodToken = "a".repeat(64);
 const server: string = "http://localhost";
 const port: string = "4001";
 
-const mnemonic: string = "your mnemonic";
-9
+const mnemonic: string = "cousin hen shallow truth chalk mind shuffle tennis cabbage culture panel stick sock mule truly between correct praise lawsuit marble hard recall mom about know";
+
 export function getClient(): algosdk.Algodv2 {
     let client = new algosdk.Algodv2(algodToken, server, port);
     return client;
