0e7a678ffd5d0f70c0e782f9d29dbb395966dc8b
Zohaib <145664940+SmollDT@users.noreply.github.com>
2024-04-28 17:21:04 +0000

    feat: additional changes to data.

--- dist/src/services/airplaneServices.js
+++ dist/src/services/airplaneServices.js
@@ -3,10 +3,10 @@ import { storeAirplaneData } from "../helpers/helpers.js";
 export const generate2000Data = () => {
     const generatedAirplaneData = {
         name: "Mikoyan Project 1.44",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -14,10 +14,10 @@ export const generate2000Data = () => {
 export const generate2001Data = () => {
     const generatedAirplaneData = {
         name: "General Atomics RQ-1 Predator B",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -25,10 +25,10 @@ export const generate2001Data = () => {
 export const generate2002Data = () => {
     const generatedAirplaneData = {
         name: "Boeing X-45",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -36,10 +36,10 @@ export const generate2002Data = () => {
 export const generate2003Data = () => {
     const generatedAirplaneData = {
         name: "Scaled Composites SpaceShipOne",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -47,10 +47,10 @@ export const generate2003Data = () => {
 export const generate2004Data = () => {
     const generatedAirplaneData = {
         name: "HESA Saeqeh",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -58,10 +58,10 @@ export const generate2004Data = () => {
 export const generate2005Data = () => {
     const generatedAirplaneData = {
         name: "ATG Javelin",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -69,10 +69,10 @@ export const generate2005Data = () => {
 export const generate2006Data = () => {
     const generatedAirplaneData = {
         name: "Lockheed Martin F-35 Lighting II",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -80,10 +80,10 @@ export const generate2006Data = () => {
 export const generate2007Data = () => {
     const generatedAirplaneData = {
         name: "Dornier S-Ray 007",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -91,10 +91,10 @@ export const generate2007Data = () => {
 export const generate2008Data = () => {
     const generatedAirplaneData = {
         name: "Scaled Composites White Knight Two",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -102,10 +102,10 @@ export const generate2008Data = () => {
 export const generate2009Data = () => {
     const generatedAirplaneData = {
         name: "Boeing 787 Dreamliner",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -113,10 +113,10 @@ export const generate2009Data = () => {
 export const generate2010Data = () => {
     const generatedAirplaneData = {
         name: "Sukhoi Su-57",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -124,10 +124,10 @@ export const generate2010Data = () => {
 export const generate2011Data = () => {
     const generatedAirplaneData = {
         name: "Chengdu J-20",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -135,10 +135,10 @@ export const generate2011Data = () => {
 export const generate2012Data = () => {
     const generatedAirplaneData = {
         name: "Boeing Phantom Eye",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -146,10 +146,10 @@ export const generate2012Data = () => {
 export const generate2013Data = () => {
     const generatedAirplaneData = {
         name: "Airbus A350",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -157,10 +157,10 @@ export const generate2013Data = () => {
 export const generate2014Data = () => {
     const generatedAirplaneData = {
         name: "AeroMobil s.r.o. AeroMobil",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -168,10 +168,10 @@ export const generate2014Data = () => {
 export const generate2015Data = () => {
     const generatedAirplaneData = {
         name: "Boeing KC-46 Pegasus",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -179,10 +179,10 @@ export const generate2015Data = () => {
 export const generate2016Data = () => {
     const generatedAirplaneData = {
         name: "Boeing T-7 Red Hawk",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -190,10 +190,10 @@ export const generate2016Data = () => {
 export const generate2017Data = () => {
     const generatedAirplaneData = {
         name: "Bell V-280 Valor",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -201,10 +201,10 @@ export const generate2017Data = () => {
 export const generate2018Data = () => {
     const generatedAirplaneData = {
         name: "Airbus BelugaXL",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -212,10 +212,10 @@ export const generate2018Data = () => {
 export const generate2019Data = () => {
     const generatedAirplaneData = {
         name: "Kratos XQ-58 Valkyrie",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -223,10 +223,10 @@ export const generate2019Data = () => {
 export const generate2020Data = () => {
     const generatedAirplaneData = {
         name: "AIDC T-5 Brave Eagle",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -234,10 +234,10 @@ export const generate2020Data = () => {
 export const generate2021Data = () => {
     const generatedAirplaneData = {
         name: "Boeing F-15 Eagle",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -245,10 +245,10 @@ export const generate2021Data = () => {
 export const generate2022Data = () => {
     const generatedAirplaneData = {
         name: "Dassault Rafale",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -256,10 +256,10 @@ export const generate2022Data = () => {
 export const generate2023Data = () => {
     const generatedAirplaneData = {
         name: "Eurofighter Typhoon",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
@@ -267,10 +267,10 @@ export const generate2023Data = () => {
 export const generate2024Data = () => {
     const generatedAirplaneData = {
         name: "Sukhoi Su-35S",
-        height: faker.number.int({ min: 5, max: 15 }),
-        width: faker.number.int({ min: 5, max: 10 }),
-        length: faker.number.int({ min: 15, max: 30 }),
-        max_speed: faker.number.int({ min: 65, max: 75 }),
+        height: faker.number.int({ min: 8000, max: 10000 }),
+        width: faker.number.int({ min: 5, max: 6 }),
+        length: faker.number.int({ min: 40, max: 50 }),
+        max_speed: faker.number.int({ min: 860, max: 1060 }),
     };
     storeAirplaneData(generatedAirplaneData).catch(console.error);
     return generatedAirplaneData;
